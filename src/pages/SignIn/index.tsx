fb9def37ccf335fd022a88273773670565c1ec6d
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-04-04 15:31:25 -0300

    feat: Estrutura SignIn criada

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { FiLogIn } from 'react-icons/fi';
 import { AlertContent, Container, Content, SliderContent } from './style';
+import { ReactComponent as GoogleIcon } from '../../assets/googleIcon.svg';
 
-const SignIn: React.FC = () => (
-  <>
-    <AlertContent> </AlertContent>
-    <Container>
-      <SliderContent> </SliderContent>
-      <Content>
-        <a href="test">Invision</a>
-        <h1>Welcome to Invision</h1>
-        <form>
-          <input
-            name="email"
-            placeholder="carolinagalvaosantos@gmail.com"
-            type="text"
-          />
-          <input
-            name="password"
-            placeholder="Digite sua senha"
-            type="password"
-          />
-          <a href="test">Forgot password?</a>
-          <button type="submit">Sign In</button>
-          <div className="divider">Or</div>
-          <button className="buttonSocialGoogle" type="button">
-            Sign In with Google
-          </button>
+const SignIn: React.FC = () => {
+  const [logged, setLogged] = useState(false);
 
+  const handleAlert = useCallback(() => {
+    setLogged(!logged);
+  }, [logged]);
+
+  return (
+    <>
+      {logged && (
+        <AlertContent>
           <div>
-            New <b>Invision</b> <a href="/signup">Create Account</a>
+            <h2>Login Realizado</h2>
+            <button type="submit" onClick={handleAlert}>
+              <FiLogIn size={55} />
+            </button>
           </div>
-        </form>
-      </Content>
-    </Container>
-  </>
-);
+        </AlertContent>
+      )}
+      <Container>
+        <SliderContent> </SliderContent>
+        <Content>
+          <a href="test">Invision</a>
+          <h1>Welcome to Invision</h1>
+          <form>
+            <input
+              name="email"
+              placeholder="carolinagalvaosantos@gmail.com"
+              type="text"
+            />
+            <input
+              name="password"
+              placeholder="Digite sua senha"
+              type="password"
+            />
+            <a href="test">Forgot password?</a>
+            <button type="submit">Sign In</button>
+            <div className="divider">Or</div>
+            <button className="buttonSocialGoogle" type="button">
+              <GoogleIcon />
+              Sign In with Google
+            </button>
+            <div>
+              New <b>Invision</b> <a href="/signup">Create Account</a>
+            </div>
+          </form>
+        </Content>
+      </Container>
+    </>
+  );
+};
 
 export default SignIn;
--- src/pages/SignIn/style.tsx
+++ src/pages/SignIn/style.tsx
@@ -1,9 +1,193 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const AlertContent = styled.div``;
+export const AlertContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-export const Container = styled.div``;
+  position: fixed;
+  z-index: 20;
 
-export const Content = styled.div``;
+  width: 100vw;
+  height: 100vh;
 
-export const SliderContent = styled.div``;
+  background: rgba(0, 0, 0, 0.64);
+
+  animation: bgOpacity 0.5s;
+
+  @keyframes bgOpacity {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  & div {
+    background: var(--white);
+
+    padding: 32px 64px;
+
+    border-radius: 5px;
+
+    animation: AlertContentUpToDown 1s;
+
+    @keyframes AlertContentUpToDown {
+      from {
+        margin-top: -100%;
+      }
+      to {
+        margin-top: 0;
+      }
+    }
+
+    & > h3 {
+      font: var(--h2);
+      color: var(--black);
+    }
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+
+  @media (min-width: 1440px) {
+    & {
+      display: flex;
+      align-items: center;
+      height: 100vh;
+      width: 100vw;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 6%;
+  max-width: 960px;
+  width: 100%;
+
+  & > a {
+    margin-left: auto;
+    margin-bottom: 72px;
+
+    font: var(--logo);
+    color: var(--black);
+  }
+
+  h1 {
+    margin-bottom: 88px;
+
+    font: var(--h1);
+    color: var(--gray);
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    max-width: 432px;
+    width: 100%;
+
+    & > a {
+      margin-left: auto;
+      margin-bottom: 30px;
+
+      font: var(--bodySmal);
+      color: var(--gray);
+    }
+
+    & > .divider {
+      display: flex;
+      align-items: center;
+
+      margin: 30px auto 30px;
+
+      font: var(--bodySmail);
+      color: var(--gray);
+
+      &::after {
+        content: '';
+
+        display: block;
+
+        width: 115px;
+        height: 0.5px;
+
+        margin-left: 20px;
+
+        background: var(--gray);
+      }
+
+      &::before {
+        content: '';
+
+        display: block;
+
+        width: 115px;
+        height: 0.5px;
+
+        margin-right: 20px;
+
+        background: var(--gray);
+      }
+    }
+
+    button.buttonSocialGoogle {
+      display: flex;
+      align-items: center;
+
+      margin: 0 auto;
+      padding: 12px 30px;
+
+      background: var(--white);
+      border: none;
+      border-radius: 100px;
+      box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.16);
+      transition: background-color 0.2s;
+
+      font: var(--buttonSocialLogin);
+
+      & svg {
+        margin-right: 20px;
+      }
+
+      &:hover {
+        background: ${shade(0.05, '#fff')};
+      }
+    }
+
+    & div:last-child {
+      margin: 40px auto 0;
+
+      font: var(--bodySmail);
+      color: var(--gray);
+
+      a {
+        color: var(--blue);
+        text-decoration: underline;
+      }
+    }
+  }
+`;
+
+export const SliderContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  background-color: var(--blue);
+
+  @media (max-width: 1024px) {
+    & {
+      display: none;
+    }
+  }
+`;
